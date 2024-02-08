5f681f1fe9f48618863b5c44c3c5498eb9573526
Siddhi <bandivdekarsiddhi@gmail.com>
2024-02-08 12:46:14 +0530

    add / remove functionality

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import products from "../data/products.json";
 import { useParams } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
 
 const Category = () => {
   const { categoryId } = useParams();
   const catProducts = products.filter((prod) => prod.categoryId === categoryId);
+  const { addToCart } = useContext(CartContext);
+
+  const [deliveryOnly, setDeliveryOnly] = useState(false);
+
+  const toggleDeliveryFilter = () => {
+    setDeliveryOnly(!deliveryOnly);
+  };
+
+  const handleAddToCart = (productId) => {
+    addToCart(productId);
+  };
 
   return (
     <div>
       <h1>Category</h1>
+      <label>
+        <input
+          type="checkbox"
+          checked={deliveryOnly}
+          onChange={toggleDeliveryFilter}
+        />
+        Delivery Only
+      </label>
       <ul>
-        {catProducts.map((product) => (
-          <li key={product.id}>
-            <img src={product.thumbnail} alt={product.name} />
-            <p>{product.name}</p>
-            <p>
-              {product.price} {product.currency}
-            </p>
-            <button>Add to cart</button>
-          </li>
-        ))}
+        {catProducts
+          .filter((product) => !deliveryOnly || product.delivery)
+          .map((product) => (
+            <li key={product.id}>
+              <img src={product.thumbnail} alt={product.name} />
+              <p>{product.name}</p>
+              <p>
+                {product.price} {product.currency}
+              </p>
+              <button onClick={() => handleAddToCart(product.id)}>
+                Add to cart
+              </button>
+            </li>
+          ))}
       </ul>
     </div>
   );
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,10 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import products from "../data/products.json";
 
 export const CartContext = createContext();
 
 const CartContextProvider = (props) => {
   const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const storedCart = localStorage.getItem("cart");
+    storedCart && setCartItems(JSON.parse(storedCart));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const addToCart = (productId) => {
     const productsToAdd = products.find((product) => product.id === productId);
@@ -23,8 +32,41 @@ const CartContextProvider = (props) => {
     }
   };
 
+  const removeFromCart = (productId) => {
+    setCartItems(cartItems.filter((item) => item.id !== productId));
+  };
+
+  const increaseQuantity = (productId) => {
+    setCartItems(
+      cartItems.map((item) =>
+        item.id === productId ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const decreaseQuantity = (productId) => {
+    const existingItem = cartItems.find((item) => item.id === productId);
+    existingItem.quantity === 1
+      ? removeFromCart(productId)
+      : setCartItems(
+          cartItems.map((item) =>
+            item.id === productId
+              ? { ...item, quantity: item.quantity - 1 }
+              : item
+          )
+        );
+  };
+
   return (
-    <CartContext.Provider value={{ cartItems, addToCart }}>
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        removeFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+      }}
+    >
       {props.children}
     </CartContext.Provider>
   );
