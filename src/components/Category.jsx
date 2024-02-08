c3b63588781f7c303c51ce09ff674dec6abc4743
Siddhi <bandivdekarsiddhi@gmail.com>
2024-02-08 11:37:55 +0530

    listed categories in each category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import products from "../data/products.json";
+import { useParams } from "react-router-dom";
 
 const Category = () => {
-  return <div>Category</div>;
+  const { categoryId } = useParams();
+  const catProducts = products.filter((prod) => prod.categoryId === categoryId);
+
+  return (
+    <div>
+      <h1>Category</h1>
+      <ul>
+        {catProducts.map((product) => (
+          <li key={product.id}>
+            <img src={product.thumbnail} alt={product.name} />
+            <p>{product.name}</p>
+            <p>
+              {product.price} {product.currency}
+            </p>
+            <button>Add to cart</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Category;
