dc346a783bcf3a9344244e13888b9095a3e24c07
Siddhi <bandivdekarsiddhi@gmail.com>
2024-02-08 11:25:15 +0530

    created components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Category from "./components/Category";
+import Checkout from "./components/Checkout";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:categoryId" element={<Category />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
