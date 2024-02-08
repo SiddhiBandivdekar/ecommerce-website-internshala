f0e80dcfccfd21b37bd982e96f3b29b74b4dedef
Siddhi <bandivdekarsiddhi@gmail.com>
2024-02-08 11:54:14 +0530

    setup context for the app

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Category from "./components/Category";
 import Checkout from "./components/Checkout";
+import CartContextProvider from "./context/CartContext";
 
 const App = () => {
   return (
-    <div>
+    <CartContextProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/category/:categoryId" element={<Category />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
-    </div>
+    </CartContextProvider>
   );
 };
 
