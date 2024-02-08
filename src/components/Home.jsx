911785158ca85af48c9842554913757d3196b207
Siddhi <bandivdekarsiddhi@gmail.com>
2024-02-08 11:30:06 +0530

    listed categories on main page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import categories from "../data/categories.json";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>Home</h1>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <Link to={`/category/${category.id}`}>{category.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
